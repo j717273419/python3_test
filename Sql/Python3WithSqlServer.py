b8da117f246ff05bd3a95992653757d161d95e3f
DESKTOP-BIMBI0O\Neusoft_Wangdongjie <717273419@qq.com>
2017-08-14 15:34:48 +0800

    练习python3读取数据库，使用pypyodbc连接sql server 2012

---
+++ Sql/Python3WithSqlServer.py
@@ -0,0 +1,29 @@
+import pypyodbc
+
+# python3使用pypyodbc读取sql server 2012的数据
+# python3 use pypyodbc read data from sql server 2012
+
+connection_string = 'Driver={SQL Server Native Client 11.0};Server=.;Database=Test;Uid=sa;Pwd=123456;'
+connection = pypyodbc.connect(connection_string)
+print(connection.connected)
+SQL = 'SELECT School_Id,School_Name FROM tb_School'
+
+cur = connection.cursor()
+result = cur.execute(SQL)
+
+count = result.rowcount
+print(count)
+
+# 只读取一行
+r = cur.fetchone()
+print("return type:", type(r))
+print("value:", r[1])
+
+# 读取所有数据
+print("fetchall:")
+r = cur.fetchall()
+for item in r:
+    print(item, end='\n')
+
+cur.close()
+connection.close()
