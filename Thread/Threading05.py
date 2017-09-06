c7c780d4abe419af87d42c0e304415b00aee160e
wdj <717273419@qq.com>
2017-09-06 14:26:23 +0800

    python3线程示例，join阻塞主线程，等待子线程运行完毕后再执行主线程

--- Thread/Threading05.py
+++ Thread/Threading05.py
@@ -12,7 +12,7 @@ def movie(func):
         sleep(5)
 
 threads = []
-t1 = threading.Thread(target=music,args=("少年二十五六时",))
+t1 = threading.Thread(target=music,args=("少年十五二十时",))
 threads.append(t1)
 t2 = threading.Thread(target=movie,args=("The Shawshank Redemption",))
 threads.append(t2)
@@ -27,3 +27,11 @@ if __name__ == '__main__':
 
 #子线程（muisc 、movie ）和主线程（print "all over %s" %ctime()）都是同一时间启动，
 # 但由于主线程执行完结束，所以导致子线程也终止。
+# 设置了子线程daemon=True才会在主线程结束的时候强制终止。。
+# 线程有一个布尔属性叫做daemon。表示线程是否是守护线程，默认取否。
+#
+# 当程序中的线程全部是守护线程时，程序才会退出。只要还存在一个非守护线程，程序就不会退出。
+#
+# 主线程是非守护线程。
+#
+# 所以如果子线程的daemon属性不是True，那么即使主线程结束了，子线程仍然会继续执行。不存在你担心的问题。只有子线程的daemon属性是True时才存在你说的问题。
---
+++ Thread/Threading07.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+from time import sleep, ctime
+import threading
+
+
+def music(func):
+    for i in range(2):
+        print('Start playing： %s! %s' % (func, ctime()))
+        sleep(2)
+
+
+def movie(func):
+    for i in range(2):
+        print('Start playing： %s! %s' % (func, ctime()))
+        sleep(5)
+
+
+def player(name):
+    r = name.split('.')[1]
+    if r == 'mp3':
+        music(name)
+    else:
+        if r == 'mp4':
+            movie(name)
+        else:
+            print('error: The format is not recognized!')
+
+lists = ['爱情买卖.mp3', '阿凡达.mp4', '金刚.mp4']
+
+threads = []
+files = range(len(lists))
+
+# 创建线程
+for i in files:
+    t = threading.Thread(target=player, args=(lists[i],))
+    threads.append(t)
+
+if __name__ == '__main__':
+    # 启动线程
+    for i in files:
+        threads[i].start()
+
+    for i in files:
+        threads[i].join()
+
+
+
+# 主线程
+print('end:%s' % ctime())
