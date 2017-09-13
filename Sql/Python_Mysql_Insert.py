0f015810a10e4f7168184edaa5f2d8bf08e0985b
wdj <717273419@qq.com>
2017-09-13 16:43:38 +0800

    python3示例， python3往mysql 插入中文

--- Sql/Python_Mysql_Insert.py
+++ Sql/Python_Mysql_Insert.py
@@ -3,11 +3,14 @@
 # 要先通过pip安装python连接mysql的库，pip install PyMysql
 import pymysql
 
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='news')
+# 解决python使用pymysql往mysql数据库插入中文时报错的问题
+# 在连接时，指明编码参数charset='utf8'
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='news',charset='utf8')
 
 cur = conn.cursor()
-sql_text = "Insert Into news_cnbeta(news_title,news_content) values('{title}','{content}')".format(title="apple",content="iphone8")
+sql_text = "Insert Into news_cnbeta(news_title,news_content) values('{title}','{content}')".format(title="小米6",content="雷军又在玩饥饿营销啦")
 cur.execute(sql_text)
+conn.commit()
 
 cur.close()
 conn.close()
