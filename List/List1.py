caac7b927b8e73bf92ab506875b89fc2a0d99a88
DESKTOP-K0A48D7\WangDongjie <717273419@qq.com>
2017-12-15 16:11:07 +0800

    python3 根据值删除List集合中的重复元素

--- List/List1.py
+++ List/List1.py
@@ -2,3 +2,14 @@ list = ["a","b",3]
 for item in list:
     print(item,end=" ")
 # print 函数的 end 关键参数来表示以空格结束输出，而不是通常的换行。
+
+# python直接获取集合中的指定信息，组合成一个新的List
+list2 = [item for item in list]
+print(list2)
+
+list2.append(3)
+print(list2)
+
+
+list3 = [item for item in list2 if item != 3]
+print(list3)
